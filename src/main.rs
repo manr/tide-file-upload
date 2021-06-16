258d493af78e8d9c8b063607ec306c87888ea5ae
ManuelR <manuel.rupp@unitedplanet.de>
2021-06-16 16:39:31 +0200

    Rename module.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use tide::Request;
 use types::AppState;
 
-mod routes;
+mod endpoints;
 mod types;
 
 #[async_std::main]
@@ -9,10 +9,10 @@ async fn main() -> tide::Result<()> {
     tide::log::start();
 
     let mut app = tide::with_state(AppState::new()?);
-    app.at("/").get(routes::get_index);
-    app.at("/file/:file").get(routes::get_file);
+    app.at("/").get(endpoints::get_index);
+    app.at("/file/:file").get(endpoints::get_file);
     app.at("/file/:file")
-        .put(move |req: Request<AppState>| routes::put_file(req));
+        .put(move |req: Request<AppState>| endpoints::put_file(req));
 
     app.listen("127.0.0.1:8080").await?;
 
